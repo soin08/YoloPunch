1fe03486a659343a16dfa1b428a9fcf626576fe1
soin08 <ilya@soin.rocks>
2015-04-22 22:05:08 +0300

    opensource-ready!

--- app/yolopunch/settings.py
+++ app/yolopunch/settings.py
@@ -7,21 +7,23 @@ https://docs.djangoproject.com/en/1.7/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
+"""
+Import the private stuff!
+"""
+try:
+    from settings_local import *
+except ImportError:
+    pass
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 YOLO_ROOT = os.path.abspath(os.path.join(BASE_DIR, os.pardir))
 
-#print(os.path.join(BASE_DIR, ".tmp"))
-#print(os.path.join(YOLO_ROOT, ".tmp"))
-#print(os.path.abspath(os.path.dirname(os.path.dirname(BASE_DIR))))
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*oyrr3hd3l+g(u1mphx*898+6*34x$o#y#x2teu0%()^$h!r#&'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
